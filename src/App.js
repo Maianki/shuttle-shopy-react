66e3f032d36c0fefbcb9e6c80d974d75918837f1
Ankit Kumain <kumankit10@gmail.com>
2022-03-02 23:22:16 +0530

    feat: add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import {Navbar} from "./components/index"
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Navbar/>
     </div>
   );
 }
