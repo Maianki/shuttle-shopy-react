4bb441a9f6cf32ce41ed5f08ba52ae4962706b1f
Ankit Kumain <kumankit10@gmail.com>
2022-03-05 00:27:02 +0530

    feat: add navbar component

---
+++ src/assets/icons/icon.js
@@ -0,0 +1,15 @@
+export function IcBaselineLogin(props) {
+    return ( 
+        <svg width = "1em"
+        height = "1em"
+        viewBox = "0 0 24 24" {
+            ...props
+        }>
+        <path fill = "currentColor" d = "M11 7L9.6 8.4l2.6 2.6H2v2h10.2l-2.6 2.6L11 17l5-5l-5-5zm9 12h-8v2h8c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-8v2h8v14z" >
+        </path> </svg>   ) }
+
+export function JustChecking(){
+    return (
+        <div>Hello World</div>
+    )
+}
