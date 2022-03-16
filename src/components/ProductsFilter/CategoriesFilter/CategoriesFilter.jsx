963029973d6c1ffd773d74189a881065900ac339
Ankit Kumain <kumankit10@gmail.com>
2022-03-16 19:59:14 +0530

    refactor: refactored the category and availability filter as per PR review

--- src/components/ProductsFilter/CategoriesFilter/CategoriesFilter.jsx
+++ src/components/ProductsFilter/CategoriesFilter/CategoriesFilter.jsx
@@ -1,36 +1,21 @@
 import React from "react";
+import { categoryFilterData } from "../../../data/category-filter-data";
 
 export function CategoriesFilter() {
   return (
     <li className='flex-column products-filter-type'>
       <h3 className='filter-heading'>Categories</h3>
-      <div className='form-check'>
-        <input type='checkbox' value='racket' id='rackets' />
-        <label className='form-label-inline' htmlFor='rackets'>
-          Rackets
-        </label>
-      </div>
 
-      <div className='form-check'>
-        <input type='checkbox' value='shuttle' id='shuttles' />
-        <label className='form-label-inline' htmlFor='shuttles'>
-          Shuttles
-        </label>
-      </div>
-
-      <div className='form-check'>
-        <input type='checkbox' value='bag' id='badminton-bags' />
-        <label className='form-label-inline' htmlFor='badminton-bags'>
-          Badminton Bags
-        </label>
-      </div>
-
-      <div className='form-check'>
-        <input type='checkbox' value='shoes' id='badminton-shoes' />
-        <label className='form-label-inline' htmlFor='badminton-shoes'>
-          Badminton Shoes
-        </label>
-      </div>
+      {categoryFilterData.map(({ id, category, value }) => {
+        return (
+          <div key={id} className='form-check'>
+            <input type='checkbox' value={value} id={value} />
+            <label className='form-label-inline' htmlFor={value}>
+              {category}
+            </label>
+          </div>
+        );
+      })}
     </li>
   );
 }
--- src/components/ProductsFilter/FilterByAvailability/FilterByAvailability.jsx
+++ src/components/ProductsFilter/FilterByAvailability/FilterByAvailability.jsx
@@ -1,29 +1,19 @@
 import React from "react";
-
+import { availabilityFilterData } from "../../../data/availability-filter-data";
 export function FilterByAvailability() {
   return (
     <li className='flex-column md-ht-1'>
       <h3 className='filter-heading'>Availability</h3>
-      <div className='form-check'>
-        <input type='checkbox' value='out-of-stock' id='out-of-stock' />
-        <label className='form-label-inline' htmlFor='out-of-stock'>
-          Include out of stock
-        </label>
-      </div>
-
-      <div className='form-check'>
-        <input type='checkbox' value='cash on delivery' id='cod' />
-        <label className='form-label-inline' htmlFor='cod'>
-          Cash on delivery
-        </label>
-      </div>
-
-      <div className='form-check'>
-        <input type='checkbox' value='all-items' id='all-items' />
-        <label className='form-label-inline' htmlFor='all-items'>
-          All items
-        </label>
-      </div>
+      {availabilityFilterData.map(({ id, availabilityType, value }) => {
+        return (
+          <div key={id} className='form-check'>
+            <input type='checkbox' value={value} id={value} />
+            <label className='form-label-inline' htmlFor={value}>
+              {availabilityType}
+            </label>
+          </div>
+        );
+      })}
     </li>
   );
 }
