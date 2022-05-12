d2d68203b39d68bd7b36e50ab77faf6615a849d8
Ankit Kumain <kumankit10@gmail.com>
2022-05-12 20:10:16 +0530

    Add responsiveness (#11) (#12)
    * refactor: moved products and category assets to cloudinery
    
    * feat: add sidebar drawer
    
    * style:add footer stick to bottom
    
    * feat: add fitlers component for smaller viewport
    
    * style: add media queries for product listing page
    
    * fix: add login link in sidebar drawer

--- src/assets/icons/icon.js
+++ src/assets/icons/icon.js
@@ -77,6 +77,21 @@ export function BiXCircle(props) {
   );
 }
 
+export function CharmMenuHamburger(props) {
+  return (
+    <svg width='2em' height='2em' viewBox='0 0 16 16' {...props}>
+      <path
+        fill='none'
+        stroke='white'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='1.5'
+        d='M2.75 12.25h10.5m-10.5-4h10.5m-10.5-4h10.5'
+      ></path>
+    </svg>
+  );
+}
+
 export function FaRegularUserCircle(props) {
   return (
     <svg width='0.97em' height='1em' viewBox='0 0 496 512' {...props}>
@@ -120,7 +135,6 @@ export function FaSolidRecycle(props) {
     </svg>
   );
 }
-
 
 export function FaSolidTruckMoving(props) {
   return (
@@ -166,5 +180,25 @@ export function IcBaselineLogin(props) {
   );
 }
 
+export function MakiCross11(props) {
+  return (
+    <svg width='1em' height='1em' viewBox='0 0 11 11' {...props}>
+      <path
+        d='M2.2 1.19l3.3 3.3L8.8 1.2a.67.67 0 0 1 .5-.2a.75.75 0 0 1 .7.7a.66.66 0 0 1-.2.48L6.49 5.5L9.8 8.82c.13.126.202.3.2.48a.75.75 0 0 1-.7.7a.67.67 0 0 1-.5-.2L5.5 6.51L2.21 9.8a.67.67 0 0 1-.5.2a.75.75 0 0 1-.71-.71a.66.66 0 0 1 .2-.48L4.51 5.5L1.19 2.18A.66.66 0 0 1 1 1.7a.75.75 0 0 1 .7-.7a.67.67 0 0 1 .5.19z'
+        fill='currentColor'
+      ></path>
+    </svg>
+  );
+}
 
 
+export function MdiFilter(props) {
+  return (
+    <svg width='1em' height='1em' viewBox='0 0 24 24' {...props}>
+      <path
+        fill='currentColor'
+        d='M14 12v7.88c.04.3-.06.62-.29.83a.996.996 0 0 1-1.41 0l-2.01-2.01a.989.989 0 0 1-.29-.83V12h-.03L4.21 4.62a1 1 0 0 1 .17-1.4c.19-.14.4-.22.62-.22h14c.22 0 .43.08.62.22a1 1 0 0 1 .17 1.4L14.03 12H14Z'
+      ></path>
+    </svg>
+  );
+}
--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,8 +1,4 @@
 import { v4 as uuid } from "uuid";
-import bag from "../../assets/images/bag.jpg";
-import rackets from "../../assets/images/rackets.jpg";
-import shoes from "../../assets/images/shoes.jpg";
-import shuttles from "../../assets/images/shuttles.jpg";
 
 /**
  * Category Database can be added here.
@@ -14,24 +10,24 @@ export const categories = [
     _id: uuid(),
     categoryName: "Rackets",
     value: "rackets",
-    img: rackets,
+    img: "https://res.cloudinary.com/dj5aind8q/image/upload/v1649613352/Shuttle%20shoppy/rackets_dirlxx.jpg",
   },
   {
     _id: uuid(),
     categoryName: "Shuttles",
     value: "shuttles",
-    img: shuttles,
+    img: "https://res.cloudinary.com/dj5aind8q/image/upload/v1649613344/Shuttle%20shoppy/shuttles_va8bnd.jpg",
   },
   {
     _id: uuid(),
     categoryName: "Badminton Bags",
     value: "badminton-bags",
-    img: bag,
+    img: "https://res.cloudinary.com/dj5aind8q/image/upload/v1649613344/Shuttle%20shoppy/bag_rpvldv.jpg",
   },
   {
     _id: uuid(),
     categoryName: "Badminton Shoes",
     value: "badminton-shoes",
-    img: shoes,
+    img: "https://res.cloudinary.com/dj5aind8q/image/upload/v1649613353/Shuttle%20shoppy/shoes_ggzvam.jpg",
   },
 ];
