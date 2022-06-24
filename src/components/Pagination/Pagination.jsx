d28badac22d2d65e98d78fcfd215e54325511818
Ankit Kumain <kumankit10@gmail.com>
2022-06-24 14:21:20 +0530

    feat: add pagination (#18)

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import "./pagination.css";
+
+export function Pagination({ RenderComponent, data, dataLimit = 6 }) {
+  const [pages] = useState(Math.ceil(data.length / dataLimit));
+  const [currentPage, setCurrentPage] = useState(1);
+
+  function goToNextPage() {
+    setCurrentPage((page) => page + 1);
+  }
+
+  function goToPreviousPage() {
+    setCurrentPage((page) => page - 1);
+  }
+
+  function changePage(event) {
+    const pageNumber = Number(event.target.textContent);
+    setCurrentPage(pageNumber);
+  }
+
+  const getPaginatedData = () => {
+    const startIndex = currentPage * dataLimit - dataLimit;
+    const endIndex = startIndex + dataLimit;
+    return data.slice(startIndex, endIndex);
+  };
+
+  const getPaginationGroup = () => {
+    let pageLimit = Math.ceil(data.length / dataLimit);
+    return new Array(pageLimit).fill().map((_, idx) => idx + 1);
+  };
+
+  console.log(getPaginationGroup());
+
+  return (
+    <div className='pagination-container'>
+      <div className='products-container'>
+        {getPaginatedData().map((product) => (
+          <RenderComponent key={product._id} product={product} />
+        ))}
+      </div>
+      <div className='pagination'>
+        {/* previous button */}
+        <button
+          onClick={goToPreviousPage}
+          className={`prev ${currentPage === 1 ? "disabled" : ""}`}
+        >
+          prev
+        </button>
+
+        {/* show page numbers */}
+        {getPaginationGroup().map((item, index) => (
+          <button
+            key={index}
+            onClick={changePage}
+            className={`paginationItem ${
+              currentPage === item ? "active" : null
+            }`}
+          >
+            <span>{item}</span>
+          </button>
+        ))}
+
+        {/* next button */}
+        <button
+          onClick={goToNextPage}
+          className={`next ${currentPage === pages ? "disabled" : ""}`}
+        >
+          next
+        </button>
+      </div>
+    </div>
+  );
+}
